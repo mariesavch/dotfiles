ff4024dbcff9a246d5b8b5781dcb589c03e9d7a8
mariesavch <mariesavch@icloud.com>
2025-06-22 22:11:38 +0500

    feat: amneziawg

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -3,4 +3,8 @@
     enable = true;
     settings.General.EnableNetworkConfiguration = true;
   };
+  networking.wg-quick.interfaces.awg0 = {
+    type = "amneziawg";
+    configFile = "/var/secrets/awg0.conf";
+  };
 }
